52fcb852873e8b671b72f74fd77d08c47263aa40
Eduardo Coppi <educoppi8@gmail.com>
2026-09-11 21:43:52 -0300

    feat: estrutura inicial do header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+export default function Header() {
+
+    return (
+        <>
+            <header>
+                <div>
+
+                </div>
+                <nav>
+                    <a href="">ESTOQUE</a>
+                    <a href="">INFO</a>
+                    <a href="">ACOMPANHAMENTO</a>
+                    <a href="">CADASTRAR TECIDO</a>
+                </nav>
+            </header>
+        </>
+    )
+}
+
