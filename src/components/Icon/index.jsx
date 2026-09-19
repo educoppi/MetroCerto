8fb520b8bc705c1651f902032ac9dee36cd48ea1
EDUARDO BRUNO COPPI <eduardo.bruno@senaisp.edu.br>
2026-09-18 21:54:09 -0300

    feat: adiciona logo e estiliza header

---
+++ src/components/Icon/index.jsx
@@ -0,0 +1,10 @@
+import Logo from '../../assets/logo.png';
+import style from './style.module.css';
+
+export default function Icon() {
+    return (
+        <div>
+            <img src={Logo} alt="" />
+        </div>
+    )
+}
